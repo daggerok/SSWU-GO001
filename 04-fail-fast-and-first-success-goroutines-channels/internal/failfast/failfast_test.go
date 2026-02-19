e80a3978973c8434acebe3d1c9787630d48886c9
Maksim Kostromin <mkostromin@sovrn.com>
2026-02-19 00:29:20 -0500

    project lead is allergic to changes... (C) whatthecommit.com

--- 04-fail-fast-and-first-success-goroutines-channels/internal/failfast/failfast_test.go
+++ 04-fail-fast-and-first-success-goroutines-channels/internal/failfast/failfast_test.go
@@ -22,12 +22,12 @@ func TestFailFast(t *testing.T) {
 		t.Errorf("FailFast should have failed due to timeout.com, but got result: %s", res)
 	} else {
 		t.Logf("FailFast correctly failed with error: %v", err)
-		errStr := err.Error()
+		errString := err.Error()
 		// Проверяем, что ошибка НЕ является ни одной из допустимых
-		isKnownError := strings.Contains(errStr, "timeout.com") ||
-			strings.Contains(errStr, "context canceled") ||
-			strings.Contains(errStr, "429") ||
-			strings.Contains(errStr, "too many requests")
+		isKnownError := strings.Contains(errString, "timeout.com") ||
+			strings.Contains(errString, "context canceled") ||
+			strings.Contains(errString, "429") ||
+			strings.Contains(errString, "too many requests")
 
 		if !isKnownError {
 			t.Errorf("Unexpected error message: '%v' doesn't contain timeout.com or context canceled", err)
